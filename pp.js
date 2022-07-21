c789afc35dc876a936ffb57d411c79ab135f778f
SanketPaharia <sanket.paharia@gmail.com>
2022-07-21 23:11:50 +0530

    productpage

---
+++ pp.js
@@ -0,0 +1,20 @@
+let productdata = [ 
+    { "img" : "https://lmsin.net/cdn-cgi/image/h=831,w=615,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000011397395-Green-SAGE-1000011397395-27062022_01-2100.jpg" , 
+     "name" : "MAX Women Printed Layered T-shirt" ,
+      "size" : "s" ,
+      "price" : 699 ,
+      "Color": "Green"
+}
+
+]
+
+
+function append() {
+
+let container = document.getElementById("bottom_sec").innerHTML
+
+let image = document.createElement('img')
+image.setAttribute('src' ,url)
+let div1 = document.createElement("div")
+
+}
