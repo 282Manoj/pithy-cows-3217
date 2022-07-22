a2f28da2adbdbaaddd762da8f739db58cec6bfe3
Mano Kumar <manojyad436@gmail.com>
2022-07-22 17:47:32 +0530

    file added

---
+++ men.js
@@ -0,0 +1,24 @@
+imgArray=["https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner1-desktop-Men-19JULY2022.png","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner6-D-Men-20JULY2022.png","https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner5-desktop-Men-18JULY2022.png","https://lmsin.net/cdn-cgi/image/w=1232,q=60,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedAD8AF927.lmsin.net/MAX-Friday/MAX2.O/MAX-Banner4-desktopp-Men-18JULY2022.png"];
+
+console.log("hi....");
+let imgNum=0;
+let img=document.querySelector("img");
+let button=document.querySelectorAll("button");
+button[0].addEventListener("click",function(){
+  if(imgNum==0){
+    imgNum=imgArray.length-1;
+  }
+  else{
+    imgNum--;
+  }
+  img.src=imgArray[imgNum];
+})
+button[1].addEventListener("click",function(){
+    if(imgNum==imgArray.length-1){
+    imgNum=0;
+  }
+  else{
+    imgNum++;
+  }
+  img.src=imgArray[imgNum];
+})
